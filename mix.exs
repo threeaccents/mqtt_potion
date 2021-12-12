e835131e57cc247b8c335f70a49654affbdf1b1d
Brian May <brian@linuxpenguins.xyz>
2021-12-12 16:23:59 +1100

    Add dependency on Jason

--- mix.exs
+++ mix.exs
@@ -27,7 +27,8 @@ defmodule MqttPotion.MixProject do
       {:emqtt, github: "emqx/emqtt", tag: "1.4.3"},
       {:ex_doc, "~> 0.24", only: :dev, runtime: false},
       {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.1.0", only: [:dev, :test], runtime: false}
+      {:dialyxir, "~> 1.1.0", only: [:dev, :test], runtime: false},
+      {:jason, "~> 1.0"}
     ]
   end
 
